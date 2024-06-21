c34cf08631af4518324534d130579de5dd5105f6
a-saber <ahs629862@gmail.com>
2024-06-21 13:08:43 -0700

    add current date time

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello, World!" + "\n" + str(datetime.datetime.now())
 
 if __name__ == "__main__":
     app.run(debug=True)
