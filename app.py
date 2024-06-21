abe4089f5cf04714614264738dec2267ad6db3c3
a-saber <ahs629862@gmail.com>
2024-06-21 13:12:14 -0700

    make date time as stream

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!" + "\n" + str(datetime.datetime.now())
+    return render_template('index.html')
+
+@app.route('/time')
+def time():
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return jsonify(time=current_time)
 
 if __name__ == "__main__":
     app.run(debug=True)
